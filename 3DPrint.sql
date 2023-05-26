f496de8009ee32e01ac8a26ec49f0f857837a211
goran-kovacic <kovacic.go@gmail.com>
2023-05-26 09:51:30 +0200

    Update 3DPrint.sql
    tables - first version

--- 3DPrint.sql
+++ 3DPrint.sql
@@ -23,3 +23,47 @@ create table user(
     password varchar(50)
 );
 
+create table print_object(
+    id int not null primary key auto_increment,
+    object_name varchar(50),
+    stl_file_original varchar(255),
+    stl_file_supported varchar(255),
+    sliced_file varchar(255),
+    project int
+);
+
+create table printer(
+    id int not null primary key auto_increment,
+    printer_name varchar(50),
+    manufacturer varchar(50),
+    printer_total_print_time time,
+    fep_count int
+);
+
+create table print_job(
+    id int not null primary key auto_increment,
+    printer int,
+    print_object int,
+    material int,
+    cost decimal(18,2),
+    result boolean
+);
+
+create table material(
+    id int not null primary key auto_increment,
+    name varchar(50),
+    manufacturer varchar(50),
+    cost_unit decimal(18,2),
+    calibrated_exposure decimal(18,2),
+    ambient_temp decimal(5,2),
+    layer_height decimal(18,2),
+    bottom_exposure decimal(18,2),
+    lift_distance int,
+    lift_speed decimal(18,2),
+    bottom_lift_distance int,
+    bottom_lift_speed decimal(18,2),
+    retract_speed decimal(18,2),
+    bottom_retract_speed decimal(18,2),
+    light_off_delay decimal(18,2)
+);
+
